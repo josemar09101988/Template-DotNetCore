c4a271d86bf089c3b9f6b1ca03a46371a0a2ea61
Josemar Silva <josemar.silva@cwi.com.br>
2022-02-16 14:29:27 -0300

    Configurado injeção de dependência e projeto IoC.

---
+++ Template/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Template.Application.Interfaces;
+
+namespace Template.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            this._userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            this._userService.Test();
+            return Ok("Ok");
+        }
+
+
+    }
+}
